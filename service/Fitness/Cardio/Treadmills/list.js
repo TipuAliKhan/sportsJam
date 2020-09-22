74e34a914c1fa93c609650321593724defbb33ec
esoteric14 <ayubayesha07@gmail.com>
2020-09-22 20:13:19 +0530

    treadmill

---
+++ service/Fitness/Cardio/Treadmills/list.js
@@ -0,0 +1,356 @@
+data = [
+  {
+    name: "WC 3777 MOTORIZED TREADMILL",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/WC3777-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/WC3777-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/WC3777-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/WC3777-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/WC3777-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/WC3777.jpg 1232w",
+    mrp: "₹ 56,000.00",
+    sellingPrice: "₹ 33,000.00",
+    productMeta: "",
+    postedIn: "Categories: Treadmill Welcare",
+  },
+  {
+    name: "WC 3333I Motorized Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3333i-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3333i-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3333i-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3333i-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3333i-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3333i-1.jpg 1232w",
+    mrp: "₹ 83,000.00",
+    sellingPrice: "₹ 50,500.00",
+    productMeta: "SKU: \n\t\t\tWC 3333I\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 4040 Rehab Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/WC-4040-Rehab-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/WC-4040-Rehab-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/WC-4040-Rehab-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/WC-4040-Rehab-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/WC-4040-Rehab-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/WC-4040-Rehab.jpg 1232w",
+    mrp: "",
+    sellingPrice: "",
+    productMeta: "SKU: \n\t\t\tWC 4040 Rehab\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 3545 M Motorized Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/WC-3545-M-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/WC-3545-M-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/WC-3545-M-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/WC-3545-M-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/WC-3545-M-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/WC-3545-M.jpg 1232w",
+    mrp: "",
+    sellingPrice: "",
+    productMeta: "SKU: \n\t\t\tWC 3545 M\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 3333 Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3333-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3333-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3333-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3333-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3333-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3333-1.jpg 1232w",
+    mrp: "₹ 69,000.00",
+    sellingPrice: "₹ 46,500.00",
+    productMeta: "SKU: \n\t\t\tWC 3333\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 2288 M Motorized Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/2288m-3-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/2288m-3-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/2288m-3-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/2288m-3-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/2288m-3-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/2288m-3.jpg 1232w",
+    mrp: "₹ 69,500.00",
+    sellingPrice: "₹ 46,500.00",
+    productMeta: "SKU: \n\t\t\tWC 2288 M\t\t",
+    postedIn: "Categories: Treadmill Welcare",
+  },
+  {
+    name: "WC 3545I Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3545i-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3545i-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3545i-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3545i-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3545i-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3545i-1.jpg 1232w",
+    mrp: "₹ 75,700.00",
+    sellingPrice: "₹ 48,000.00",
+    productMeta: "SKU: \n\t\t\tWC 3545I\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 4648I Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/4648i-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/4648i-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/4648i-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/4648i-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/4648i-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/4648i-1.jpg 1232w",
+    mrp: "₹ 89,900.00",
+    sellingPrice: "₹ 56,000.00",
+    productMeta: "SKU: \n\t\t\tWC 4648I Treadmill\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 3777M MOTORIZED TREADMILL",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/WC3777M-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/WC3777M-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/WC3777M-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/WC3777M-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/WC3777M-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/WC3777M.jpg 1232w",
+    mrp: "₹ 65,000.00",
+    sellingPrice: "₹ 36,000.00",
+    productMeta: "",
+    postedIn: "Categories: Treadmill Welcare",
+  },
+  {
+    name: "WC 4848 AC Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/4848ac-2-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/4848ac-2-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/4848ac-2-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/4848ac-2-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/4848ac-2-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/4848ac-2.jpg 1232w",
+    mrp: "₹ 109,000.00",
+    sellingPrice: "₹ 77,500.00",
+    productMeta: "SKU: \n\t\t\tWC 4848AC\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 2288I Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/2288i-3-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/2288i-3-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/2288i-3-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/2288i-3-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/2288i-3-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/2288i-3.jpg 1232w",
+    mrp: "₹ 77,000.00",
+    sellingPrice: "₹ 43,500.00",
+    productMeta: "SKU: \n\t\t\tWC 2288I\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 4646 Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/WC-4646-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/WC-4646-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/WC-4646-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/WC-4646-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/WC-4646-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/WC-4646.jpg 1232w",
+    mrp: "",
+    sellingPrice: "",
+    productMeta: "SKU: \n\t\t\tWC 4646\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 3333I M Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3333mi-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3333mi-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3333mi-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3333mi-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3333mi-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3333mi-1.jpg 1232w",
+    mrp: "₹ 83,000.00",
+    sellingPrice: "₹ 56,500.00",
+    productMeta: "SKU: \n\t\t\tWC 3333I M\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 3333 M Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3333m-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3333m-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3333m-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3333m-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3333m-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3333m-1.jpg 1232w",
+    mrp: "₹ 75,000.00",
+    sellingPrice: "₹ 53,500.00",
+    productMeta: "SKU: \n\t\t\tWC 3333 M\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 2288 Mi Motorized Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/2288mi-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/2288mi-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/2288mi-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/2288mi-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/2288mi-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/2288mi-1.jpg 1232w",
+    mrp: "₹ 79,900.00",
+    sellingPrice: "₹ 48,500.00",
+    productMeta: "SKU: \n\t\t\tWC 2288I M\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 5777 AC Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/5777ac-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/5777ac-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/5777ac-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/5777ac-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/5777ac-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/5777ac-1.jpg 1232w",
+    mrp: "₹ 181,000.00",
+    sellingPrice: "₹ 99,000.00",
+    productMeta: "SKU: \n\t\t\tWC 5777AC\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC3500I DC MOTORIZED TREADMILL",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3500i-3-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3500i-3-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3500i-3-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3500i-3-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3500i-3-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3500i-3.jpg 1232w",
+    mrp: "₹ 88,000.00",
+    sellingPrice: "₹ 54,000.00",
+    productMeta: "",
+    postedIn: "Categories: Treadmill Welcare",
+  },
+  {
+    name: "WC 4848DC Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/4848dc-2-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/4848dc-2-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/4848dc-2-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/4848dc-2-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/4848dc-2-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/4848dc-2.jpg 1232w",
+    mrp: "₹ 123,000.00",
+    sellingPrice: "₹ 72,000.00",
+    productMeta: "SKU: \n\t\t\tWC 4848DC\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC-2288 Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/2288-2-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/2288-2-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/2288-2-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/2288-2-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/2288-2-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/2288-2.jpg 1232w",
+    mrp: "₹ 63,000.00",
+    sellingPrice: "₹ 39,500.00",
+    productMeta: "SKU: \n\t\t\tWC 2288\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC 5888 Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/5888-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/5888-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/5888-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/5888-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/5888-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/5888-1.jpg 1232w",
+    mrp: "₹ 185,000.00",
+    sellingPrice: "₹ 102,000.00",
+    productMeta: "SKU: \n\t\t\tWC 5888 AC\t\t",
+    postedIn: "Category: Welcare",
+  },
+  {
+    name: "WC3500 DC MOTORIZED TREADMILL",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3500-3-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3500-3-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3500-3-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3500-3-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3500-3-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3500-3.jpg 1232w",
+    mrp: "₹ 77,000.00",
+    sellingPrice: "₹ 49,000.00",
+    productMeta: "",
+    postedIn: "Categories: Treadmill Welcare",
+  },
+  {
+    name: "WC-3555 Motorized Treadmill",
+    image:
+      "https://welcareindia.com/shop/wp-content/uploads/3555-1-600x776.jpg 600w, https://welcareindia.com/shop/wp-content/uploads/3555-1-232x300.jpg 232w, https://welcareindia.com/shop/wp-content/uploads/3555-1-791x1024.jpg 791w, https://welcareindia.com/shop/wp-content/uploads/3555-1-768x994.jpg 768w, https://welcareindia.com/shop/wp-content/uploads/3555-1-1187x1536.jpg 1187w, https://welcareindia.com/shop/wp-content/uploads/3555-1.jpg 1232w",
+    mrp: "₹ 58,000.00",
+    sellingPrice: "₹ 35,000.00",
+    productMeta: "SKU: \n\t\t\tWC 3555\t\t",
+    postedIn: "Category: Welcare",
+  },
+];
+for (i = 0; i < data.length; i++) {
+  // ./product-details/prduct name.html
+  var li = data[i];
+  var title = li["name"];
+  var image = li.image.split(",")[0].split(" ")[0];
+  //   console.log(image);
+  var desc = li["postedIn"];
+  var price = li["sellingPrice"];
+  var un = title.split(" ").join("-");
+  un = un.replace(/\//g, "-") + "-" + i;
+  var url = "./product-details/" + un + ".html";
+  var ele = document.getElementsByClassName("products product_list grid row");
+  var html =
+    ` <div class="item col-lg-4 col-md-6 col-sm-6 text-center"
+    >
+    <div
+    class="product-miniature js-product-miniature item-one"
+    data-id-product="3"
+    data-id-product-attribute="13"
+    itemscope
+    itemtype="http://schema.org/Product"
+    >
+    <div class="thumbnail-container">
+        <a
+        href="` +
+    url +
+    `" class="thumbnail product-thumbnail one-image"
+        >
+        <img
+            class="img-fluid image-cover"
+            src="` +
+    image +
+    `"
+            alt="` +
+    title +
+    `"
+            data-full-size-image-url="` +
+    image +
+    `"
+            width="450"
+            height="268"
+        />
+        </a>
+    </div>
+    <div class="product-groups">
+        <div class="product-description">
+        <div class="category-title">
+            <a
+            href="` +
+    url +
+    `"
+            ></a
+            >
+        </div>
+    
+        <div class="product-title" itemprop="name">
+            <a href="` +
+    url +
+    `">` +
+    title +
+    `</a
+            >
+        </div>
+    
+        <div
+            class="group-price d-flex align-items-center justify-content-between"
+        >
+            <div class="product-comments">
+            <div class="star_content">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+            </div>
+            <span>0 review</span>
+            </div>
+            <div class="product-group-price">
+            <div class="product-price-and-shipping">
+                <span itemprop="price" class="price"
+                >` +
+    price +
+    `</span
+                >
+            </div>
+            </div>
+        </div>
+        <div class="product-desc">
+        ` +
+    title +
+    `
+        </div>
+        </div>
+        <div
+        class="product-buttons d-flex justify-content-center"
+        itemprop="offers"
+        itemscope
+        itemtype="http://schema.org/Offer"
+        >
+        <a
+            class="addToWishlist"
+            href="#"
+            data-rel="3"
+            onclick="WishlistCart('wishlist_block_list', 'add', '3', false, 1, '', '` +
+    title +
+    `', '` +
+    title +
+    `.html', '` +
+    image +
+    `'); return false;"
+        >
+            <i class="fa fa-heart-o"></i>
+            <span>Add to Wishlist</span>
+        </a>
+        <form
+            action="http://localhost/sportsjam/en/cart"
+            method="post"
+        >
+            <input
+            type="hidden"
+            name="token"
+            value="380c4d7aea12c17aa9fc669ea4cf2be3"
+            />
+            <input
+            type="hidden"
+            name="id_product"
+            value="3"
+            />
+            <input type="hidden" name="qty" value="1" />
+            <a
+            class="add-to-cart show_popup has-text align-self-center"
+            href="#"
+            data-button-action="add-to-cart"
+            ><span class="loading"
+                ><i
+                class="fa fa-spinner fa-spin"
+                aria-hidden="true"
+                ></i></span
+            ><span>Add To Cart</span
+            ><i class="fa fa-cart-plus"></i
+            ></a>
+        </form>
+        <a
+            href="#"
+            class="quick-view hidden-sm-down"
+            data-link-action="quickview"
+        >
+            <i class="fa fa-search"></i
+            ><span> Quick view</span>
+        </a>
+        </div>
+    </div>
+    </div>
+    </div>`;
+  ele[0].insertAdjacentHTML("afterbegin", html);
+}
