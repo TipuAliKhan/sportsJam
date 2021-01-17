fdcf86ebfb9472b39fb553dbcb12ad0375a3af2c
Tipu <tipu.alikhan@nowfloats.com>
2021-01-17 19:11:43 +0530

    checkout fixes for payment

--- scripts/cartifyPurchase.js
+++ scripts/cartifyPurchase.js
@@ -1,28 +1,3 @@
-function CartifyPurchase() {
-  this.gateway = "INSTAMOJO"
-}
-CartifyPurchase.prototype.setGateway = function (gateway = "INSTAMOJO") {
-  this.gateway = gateway;
-};
-
-CartifyPurchase.prototype.initPayment = function (cartifyOrderObject, uniquePaymentId, payableAmount, merchantName, description) {
-  let order = cartifyOrderObject;
-  let cart = [];
-
-  order.orderItems.forEach(function (item) {
-    cart.push({
-      "ProductOrOfferId": item._id,
-      "Type": "PRODUCT",
-      "currency": item.currency,
-      "image": item.image,
-      "name": item.name,
-      "price": item.price,
-      "discount": item.discount,
-      "quantity": item.quantity
-    });
-  });
-};
-
 function placeOrder(itemsToBuy, buyerDetail) {
   var data = JSON.stringify({
     "items": itemsToBuy,
@@ -43,7 +18,8 @@ function placeOrder(itemsToBuy, buyerDetail) {
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
       console.log(this.responseText);
-      window.open(this.responseText.paymentUrl);
+      debugger;
+      window.open(JSON.parse(this.responseText).paymentUrl);
     }
   });
 
@@ -102,7 +78,87 @@ function checkout() {
   }
   
 }
-(function () {
+
+function populateCartDetail(){
+  let productListTemplate = '';
+  const allItemInCart = cartify.getAllItems();
+  allItemInCart.forEach((e)=>{
+    productListTemplate += `<li class="media">
+    <div class="media-left d-flex">
+        <img class="media-object"
+          src="${e.image}"
+          alt="${e.name.substr(0,20)}">
+    </div>
+    <div class="media-body d-lg-flex justify-content-lg-between">
+      <div class="product-name">${e.name}</div>
+      <div>
+        <div class="product-price pull-xs-right">₹${e.price - e.discount}</div>
+        <div class="product-quantity">x${e.quantity}</div>
+      </div>
+    </div>
+  </li>`;
+  })
+  const cartSummary = `
+  <div class="cart-summary-products">
+    <div class="summary-label d-flex align-items-center">
+      <span>There are 1 item in your cart</span>
+      <span class="show-details ml-auto">
+        <a href="#" data-toggle="collapse" data-target="#cart-summary-product-list">
+        </a>
+      </span>
+    </div>
+    <div class="collapse in" id="cart-summary-product-list">
+      <ul class="media-list">
+        ${productListTemplate}
+      </ul>
+    </div>
+  </div>`;
+  const priceTemplate = `<div class="group-price">
+    <div class="cart-summary-line" id="cart-subtotal-products">
+      <span class="label js-subtotal">
+        Total products:
+      </span>
+      <span class="value">₹${cartify.getPayableAmount()}</span>
+    </div>
+    <div class="cart-summary-line" id="cart-subtotal-shipping">
+      <span class="label">
+        Total Shipping:
+      </span>
+      <span class="value">₹0.00</span>
+      <div><small class="value"></small></div>
+    </div>
+    <div class="cart-summary-line cart-tax">
+      <span class="label sub">Taxes:</span>
+      <span class="value sub">₹0.00</span>
+    </div>
+  </div>`;
+
+  const totalPrice = `
+  <div class="cart-summary-totals">
+    <div class="cart-summary-line cart-total has_border">
+      <div class="d-flex">
+        <span>
+          <span class="label">Total</span>&nbsp;<span class="font-small">(tax
+            excl.)</span>
+        </span>
+        <span class="value label ml-auto">₹${cartify.getPayableAmount()}</span>
+      </div>
+    </div>
+    <button id="sportjam-checkout" class="btn btn-primary"
+      style="font-size: 16px;padding: 14px 25px;width: 100%;">
+      Continue
+    </button>
+  </div>`;
+
+  cartDetailTemplate = cartSummary + priceTemplate + totalPrice;
+  cartDetailTemplateContainer = document.getElementById('js-checkout-summary');
+  cartDetailTemplateContainer.innerHTML = cartDetailTemplate;
+
   const checkoutBtn = document.getElementById('sportjam-checkout');
   checkoutBtn.addEventListener('click', checkout);
+}
+
+
+(function () {
+  populateCartDetail();
 })();
